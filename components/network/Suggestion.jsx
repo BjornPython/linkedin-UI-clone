95b1df57e95a975527111b90c9641cc67c49e29a
Nathan <nathanflores887@gmail.com>
2023-03-27 12:54:55 +0800

    Fixed layout for my network.

---
+++ components/network/Suggestion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Suggestion({ styles }) {
+    return (
+        <div className={styles.suggestionDiv}>Suggestion</div>
+    )
+}
+
+export default Suggestion
--- components/network/Suggestions.jsx
+++ components/network/Suggestions.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Suggestion from './Suggestion'
+
 
 function Suggestions({ styles }) {
     return (
         <div className={styles.suggestionsContainer}>
             <p>Suggestions</p>
+            <div className={styles.allSuggestions}>
+                <Suggestion styles={styles} />
+                <Suggestion styles={styles} />
+                <Suggestion styles={styles} />
+                <Suggestion styles={styles} />
+                <Suggestion styles={styles} />
+                <Suggestion styles={styles} />
+
+            </div>
         </div>
     )
 }

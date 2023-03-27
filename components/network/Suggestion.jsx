9cf9834eec52d66d2ef0bf5b159e2db4316691aa
Nathan <nathanflores887@gmail.com>
2023-03-27 14:16:10 +0800

    Added designs for suggestion/

--- components/network/Suggestion.jsx
+++ components/network/Suggestion.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import Image from "next/image"
+import banner from "../../public/images/banner.png"
+import Circle from "../../public/svgs/circle.svg"
 function Suggestion({ styles }) {
     return (
-        <div className={styles.suggestionDiv}>Suggestion</div>
+        <div className={styles.suggestionDiv}>
+            <Image src={banner} alt="" className={styles.suggestionBanner} />
+
+            <div className={styles.suggestionInfo}>
+                <Circle className={styles.sgUserIcn} />
+                <p>Nathan Flores</p>
+                <p>Bio</p>
+
+                <div className={styles.followBtn}>
+                    <p>+ Follow</p>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
